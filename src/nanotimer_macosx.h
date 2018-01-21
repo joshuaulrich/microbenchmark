4333c3e12c3ab7d87050b228213a970f9d91064b
Joshua Ulrich <josh.m.ulrich@gmail.com>
2018-01-20 18:52:15 -0600

    Only call mach_timebase_info() once
    The example Apple provides caches the result of mach_timebase_info().
    Do the same, so we don't make the same function call for every call
    to get_nanotime().
    
    Note the potential for lost precision if the integer division would
    produce a quotient with a (truncated) remainder.
    
    [1] https://developer.apple.com/library/content/qa/qa1398/_index.html

--- src/nanotimer_macosx.h
+++ src/nanotimer_macosx.h
@@ -1,13 +1,30 @@
 #include <mach/mach_time.h>
 
-/* see http://developer.apple.com/library/mac/#qa/qa2004/qa1398.html */
+/* QA1398 [1] warns that this calculation may overflow uint64:
+ * mach_absolute_time() * mach_timebase_info.numer / mach_timebase_info.denom
+ *
+ * The StackOverflow question [2] suggests that it's mainly a problem on
+ * Power-PC Macs, since the numerator and denominator are both 1 on Intel
+ * Macs.
+ *
+ * [1] https://developer.apple.com/library/content/qa/qa1398/_index.html
+ * [2] https://stackoverflow.com/questions/23378063
+ */
 static nanotime_t get_nanotime(void) {
-    uint64_t time;
-    mach_timebase_info_data_t info;
-    
-    time = mach_absolute_time();
-    mach_timebase_info(&info);
-    
+    static uint64_t ratio = 0;
+    if (ratio == 0) {
+        mach_timebase_info_data_t info;
+        mach_timebase_info(&info);
+        if ((info.numer % info.denom) == 0) {
+            ratio = info.numer / info.denom;
+        } else {
+            warning("less accurate nanosecond times to avoid potential integer overflows");
+            ratio = (uint64_t)((double)info.numer / info.denom);
+        }
+    }
+
+    uint64_t time = mach_absolute_time();
+
     /* Convert to nanoseconds */
-    return (nanotime_t)(time * (info.numer / info.denom));
+    return (nanotime_t)(time * ratio);
 }
